ce4d7c845447530e87c59135423d3f9bf2e7e54e
aao0007 <aao0007@alu.medac.es>
2026-05-24 01:28:26 +0200

    fotos

--- obj/Debug/net8.0-windows/EldenRingArmorStudio.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/EldenRingArmorStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EldenRingArmorStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f680376ae513e25bd33cafc37e76b22adc257ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ecd768229824374d7f9edc17b2f24b6efc3735")]
 [assembly: System.Reflection.AssemblyProductAttribute("EldenRingArmorStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EldenRingArmorStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
