4aede12ff60fa25df83b79f7eb3d055738dea755
aao0007 <aao0007@alu.medac.es>
2026-05-25 03:55:41 +0200

    s

--- obj/Debug/net8.0-windows/EldenRingArmorStudio.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/EldenRingArmorStudio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EldenRingArmorStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a590d60dad6c8da05a113ac55e98a01aaa8c5a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30d1746bf329e1d6fab9619b9b44e17a4ece22c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EldenRingArmorStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EldenRingArmorStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0-windows/UI/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/UI/MainWindow.g.i.cs
@@ -99,7 +99,7 @@ namespace EldenRingArmorStudio.UI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/EldenRingArmorStudio;component/ui/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/EldenRingArmorStudio;V1.0.0.0;component/ui/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\UI\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
